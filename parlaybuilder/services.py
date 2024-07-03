39f15340520b26638dcc66ac559cf10d8b1cd0e6
mert <mertozka41@gmail.com>
2024-07-03 12:14:19 +0300

    url for last 5 games changed

--- parlaybuilder/services.py
+++ parlaybuilder/services.py
@@ -1,40 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-team_abbreviations = {
-        "ATL": "hawks",
-        "BOS": "celtics",
-        "BRK": "nets",
-        "CHO": "hornets",
-        "CHI": "bulls",
-        "CLE": "cavs",
-        "DAL": "mavs",
-        "DEN": "nuggets",
-        "DET": "pistons",
-        "GSW": "warriors",
-        "HOU": "rockets",
-        "IND": "pacers",
-        "LAC": "clippers",
-        "LAL": "lakers",
-        "MEM": "grizzlies",
-        "MIA": "heat",
-        "MIL": "bucks",
-        "MIN": "timberwolves",
-        "NOP": "pelicans",
-        "NY": "knicks",
-        "ORL": "magic",
-        "OKC": "thunder",
-        "PHI": "sixers",
-        "PHO": "suns",
-        "POR": "blazers",
-        "SAS": "spurs",
-        "SAC": "kings",
-        "TOR": "raptors",
-        "UTA": "jazz",
-        "WAS": "wizards"
-}
+
 def GetStats(first_name, last_name):
     season_stats = GetSeasonStats(first_name, last_name)
+    last_five_stats = StatMuseData(f"https://www.statmuse.com/nba/ask/{first_name}-{last_name}-stats-last-5-games")
+    season_stats["last5"] = last_five_stats
     # print(season_stats)
     # if  "season_stats" in season_stats:
     #     opponent_stats = GetNextOpponent(first_name, last_name, season_stats["next_team_abr"])
@@ -87,7 +58,6 @@ def GetTodaysGames():
     
     
 def GetSeasonStats(first_name, last_name):
-    # 02 for jaylen brown, anthony davis
     index = ""
     full_name = first_name + " " + last_name
     if full_name == "jaylen brown":
@@ -102,7 +72,7 @@ def GetSeasonStats(first_name, last_name):
         index = "6"
     else:
         index = "1"
-        
+    
     response = requests.get(f'https://www.basketball-reference.com/players/{last_name[0]}/{last_name[:5]}{first_name[:2]}0{index}.html')
     soup = BeautifulSoup(response.content, 'html.parser')
     all_stats = soup.find(class_="p1")
@@ -118,25 +88,7 @@ def GetSeasonStats(first_name, last_name):
         picdiv = soup.find(class_="media-item")
         pic = picdiv.find_all("img")
         season_stats = {"pts": points_this_season, "assist": assists_this_season, "reb": rebounds_this_season}
-        # LAST 5 GAMES
-        nu_row = []
-        table = soup.find('table', id="last5")
-        rows = table.find_all('tr')
-        rows.pop(0)
-        for row in rows:
-            cols = row.find_all(['td', 'th'])
-            cols = [ele.text.strip() for ele in cols]
-            date_sr = pd.to_datetime(pd.Series(cols[0]))
-            updated_date = date_sr.dt.strftime('%d/%m/%Y')
-            nu_row.append([updated_date[0],cols[3],cols[6],cols[7],cols[8],cols[10],cols[11],cols[18], cols[19], cols[20], cols[21], cols[24]])
-        df1 = pd.DataFrame(nu_row, columns=['Date', 'Opp', 'MP', 'FG', 'FGA', '3P', '3PA', 'REB', 'AST', 'STL', 'BLK', 'PTS'])
-        # NEXT OPP ABR
-        # footer = soup.find('div', id="tfooter_last5")
-        # next = footer.find_all("a")
-
-        # team = next[0].get_text()
-        # "next_team_abr": team
-        return {"img": pic[0]["src"], "season_stats": season_stats, "last5": df1}
+        return {"img": pic[0]["src"], "season_stats": season_stats}
 
 def GetTeamPlayers(team):
     rows = GetTable(f'https://www.basketball-reference.com/teams/{team}/2024.html', "per_game")
@@ -150,10 +102,13 @@ def GetTeamPlayers(team):
     return {"roster": df1}
 
 def GetNextOpponent(first_name, last_name, team):
-    # team_name = team_abbreviations[team]
-    stat_response = requests.get(f'https://www.statmuse.com/nba/ask?q={first_name}+{last_name}+last+5+games+vs+{team}')
-    soupy = BeautifulSoup(stat_response.content, 'html.parser')
-    opp_table = soupy.find("table")
+    stats = StatMuseData(f'https://www.statmuse.com/nba/ask?q={first_name}+{last_name}+last+5+games+vs+{team}')
+    return stats
+
+def StatMuseData(url):
+    stat_response = requests.get(url)
+    soup = BeautifulSoup(stat_response.content, 'html.parser')
+    opp_table = soup.find("table")
     opp_rows = opp_table.find_all("tr")
     opp_rows.pop(0)
     nu_row = []
@@ -165,5 +120,5 @@ def GetNextOpponent(first_name, last_name, team):
     nu_row.reverse()
     nu_row.pop(0)
     nu_row.pop(0)
-    df2 = pd.DataFrame(nu_row, columns=['Date', 'Opp', 'MP', 'PTS', 'REB', 'AST','STL', 'BLK', 'FG', 'FGA', '3P', '3PA'])
-    return df2
+    df1 = pd.DataFrame(nu_row, columns=['Date', 'Opp', 'MP', 'PTS', 'REB', 'AST','STL', 'BLK', 'FG', 'FGA', '3P', '3PA'])
+    return df1
