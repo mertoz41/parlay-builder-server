d5a2d7c9dcf8f7d65d34fbadc18795642e2ea87a
mert <mertozka41@gmail.com>
2024-07-12 13:13:50 +0300

    pandas used rather than beautiful soup for table scraping

--- parlaybuilder/services.py
+++ parlaybuilder/services.py
@@ -1,29 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-common_columns = ["FGM", "FGA", "3PM", "3PA", "REB", "AST", "STL", "BLK", "PTS"]
 
 def GetStats(first_name, last_name):
     season_stats = GetSeasonStats(first_name, last_name)
     last_five_stats = StatMuseData(f"https://www.statmuse.com/nba/ask/{first_name}-{last_name}-stats-last-5-games")
     season_stats["last5"] = last_five_stats
-    # print(season_stats)
-    # if  "season_stats" in season_stats:
-    #     opponent_stats = GetNextOpponent(first_name, last_name, season_stats["next_team_abr"])
-    #     response = {**season_stats, **opponent_stats}
-    # else: 
-    #     response = {"error": season_stats["error"]}
-    
     return season_stats
-
-# search player functionality (incorporating Selenium for search interaction)
-# find the search form, input clients input from front end and present options
-
-# def SearchPlayer():
-#     browser = webdriver.C
-
 
 def GetAllTeams():
     response = requests.get("https://www.nba.com/teams")
@@ -36,35 +19,17 @@ def GetAllTeams():
     return cleaned_teams
 
 def GetMvpList():
-    rows = GetTable('https://www.basketball-reference.com/friv/mvp.html', "players")
-    top5 = rows[:5]
-    new_rows = []
-    for row in top5:
-        cols = row.find_all(['td', 'th'])
-        cols = [ele.text.strip() for ele in cols]
-        new_rows.append([cols[0], cols[1], cols[2], cols[3], cols[4], cols[9], cols[10], cols[12], cols[13], cols[24], cols[25], cols[26], cols[27], cols[30]])
-    df1 = pd.DataFrame(new_rows, columns=['Rank', 'Player', 'Team', 'W', 'L'] + common_columns)
-    return df1
+    dfs = pd.read_html('https://www.basketball-reference.com/friv/mvp.html')
+    df = dfs[0][0:5]
+    df = df[["Rk", "Player", "Team","W", "L", "FG", "FGA", "3P", "3PA", "TRB", "AST", "STL", "BLK", "PTS"]]
+    df.rename(columns={'FG': "FGM", "3P": "3PM", "Rk": "Rank", "TRB": "REB"}, inplace=True)
+    return df
 
 
-def GetTable(url, table_id):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find('table', id=table_id)
-    rows = table.find_all("tr")
-    rows.pop(0)
-    return rows
+# def GetTodaysGames():
+#     will be revisited once season starts
+#     'https://www.foxsports.com/nba/schedule'
 
-
-def GetTodaysGames():
-    rows = GetTable('https://www.foxsports.com/nba/schedule', "table-0")
-    nu_row = []
-    for row in rows:
-        cols = row.find_all(['td', 'th'])
-        imgs = [ele.img for ele in cols]
-        cols = [ele.text.strip() for ele in cols]
-        nu_row.append([cols[0], imgs[0]['src'], cols[2], imgs[2]['src']])
-    return nu_row
     
     
 def GetSeasonStats(first_name, last_name):
@@ -96,15 +61,11 @@ def GetSeasonStats(first_name, last_name):
         return {"img": pic[0]["src"], "season_stats": season_stats}
 
 def GetTeamPlayers(team):
-    rows = GetTable(f'https://www.basketball-reference.com/teams/{team}/2024.html', "per_game")
-    top8 = rows[:9]
-    new_rows = []
-    for row in top8:
-        cols = row.find_all(['td', 'th'])
-        cols = [ele.text.strip() for ele in cols]
-        new_rows.append([cols[1], cols[6], cols[7], cols[9], cols[10], cols[21], cols[22], cols[23], cols[24], cols[27]])
-    df1 = pd.DataFrame(new_rows, columns=['Player'] + common_columns)
-    return {"roster": df1}
+    dfs = pd.read_html(f'https://www.basketball-reference.com/teams/{team}/2024.html')
+    df = dfs[1][0:9]
+    df = df[["Player", "FG", "FGA", "3P", "3PA", "TRB", "AST", "STL", "BLK", "PTS"]]
+    df.rename(columns={'FG': "FGM", "3P": "3PM", "TRB": "REB"}, inplace=True)
+    return {"roster": df}
 
 def GetNextOpponent(first_name, last_name, team):
     stats = StatMuseData(f'https://www.statmuse.com/nba/ask?q={first_name}+{last_name}+last+5+games+vs+{team}')
@@ -113,13 +74,15 @@ def GetNextOpponent(first_name, last_name, team):
 def StatMuseData(url):
     dfs = pd.read_html(url)
     df = dfs[0][0:5]
-    dropped_columns = df.drop(df.columns[[0,1,5]], axis=1)
-    updated_df = dropped_columns.drop(["NAME", "TM", "FG%", "3P%", "FTM", "FTA", "FT%", "TS%", "OREB", "DREB", "TOV", "PF", "+/-"], axis=1)
+    df = df.drop(df.columns[[0,1,5]], axis=1)
+    df = df[["DATE", "OPP", "MIN", "FGM", "FGA", "3PM", "3PA", "STL", "BLK", "AST", "PTS", "REB"]]
+    df.rename(columns={'DATE': "Date"}, inplace=True)
+
     types = {}
-    for col in updated_df.columns:
-        if updated_df[col].dtype == "float64":
+    for col in df.columns:
+        if df[col].dtype == "float64":
             types[col] = "int"
         else:
-            types[col] = updated_df[col].dtype
-    df = updated_df.astype(types)
+            types[col] = df[col].dtype
+    df = df.astype(types)
     return df
--- parlaybuilder/views.py
+++ parlaybuilder/views.py
@@ -1,13 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from rest_framework.parsers import JSONParser
 from .services import GetStats, GetAllTeams, GetMvpList, GetTeamPlayers, GetNextOpponent
 
 @api_view(['GET', 'POST'])
 def index(request):
     if request.method == "GET":
-        # todays_games = GetTodaysGames()
         all_teams = GetAllTeams()
         mvp_list = GetMvpList()
         return Response({"all_teams": all_teams, "mvp_list": mvp_list})
@@ -21,7 +19,6 @@ def index(request):
         response = GetStats(first_name, last_name) 
         return Response(response)
 
-        # parsed = JSONParser().parse(request)
 
 @api_view(['GET'])
 def get_team(request, team):
@@ -34,14 +31,5 @@ def get_opponent_stats(request):
     opponent_stats = GetNextOpponent(parsed["first"], parsed["last"], parsed["team"])
     return Response({"opp_stats": opponent_stats})
 
-# def search_player(request):
-#     parsed = JSONParser().parse(request)
-#     splitted_full_name = parsed["player"].split()
-#     if len(splitted_full_name) < 2:
-#         return Response({"error": "Player not in the league."})
-#     first_name = splitted_full_name[0]
-#     last_name = splitted_full_name[1]
-#     response = GetStats(first_name, last_name) 
-#     return Response(response)
 
 
